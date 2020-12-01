bbcdb81354ebc28090e55794a594c575d5b08f82
sumitvairagar <46483045+sumitvairagar@users.noreply.github.com>
2020-12-01 11:08:24 +0530

    binary search

---
+++ src/com/sumit/grooky/BinarySearch.kt
@@ -0,0 +1,14 @@
+package com.sumit.grooky
+
+
+
+
+
+fun main() {
+    val sorted = listOf(3, 6, 8, 10, 14, 19, 23, 30)
+    println("Searched the number: ${binarySearch(sorted, 14)}")
+}
+
+fun binarySearch(sorted: List<Int>, i: Int): String {
+    
+}
